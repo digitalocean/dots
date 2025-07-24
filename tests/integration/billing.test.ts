1da7a8429b1f9c4956c8a6ea9cddae6582307fb9
mchittupolu <mchittupolu@digitalocean.com>
2025-07-24 16:05:48 -0700

    Added few integration tests

--- tests/integration/billing.test.ts
+++ tests/integration/billing.test.ts
@@ -3,7 +3,7 @@ import { FetchRequestAdapter } from "@microsoft/kiota-http-fetchlibrary";
 import { createDigitalOceanClient } from "../../src/dots/digitalOceanClient.js";
 import { DigitalOceanApiKeyAuthenticationProvider } from "../../src/dots/DigitalOceanApiKeyAuthenticationProvider.js";
 import * as fs from "fs";
-const invoiceUuid = "mock-invoice-uuid";
+const invoiceUuid = "something";
 const baseUrl = 'https://api.digitalocean.com'
 import dotenv from "dotenv";
 dotenv.config();
@@ -24,11 +24,12 @@ describe("Integration Test for Billing", () => {
     it("should get customer balance", async () => {
         const expectedResponse = { account_balance: "0.00" };
 
-        nock(baseUrl)
-            .get("/v2/customers/my/balance")
-            .reply(200, expectedResponse);
+        // nock(baseUrl)
+        //     .get("/v2/customers/my/balance")
+        //     .reply(200, expectedResponse);
 
         const getResp = await client.v2.customers.my.balance.get();
+        console.log(getResp);
         expect(getResp).not.toBeNull();
         expect(getResp?.accountBalance).toBe("0.00");
     });
@@ -41,9 +42,9 @@ describe("Integration Test for Billing", () => {
             ],
         };
 
-        nock(baseUrl)
-            .get("/v2/customers/my/billing_history")
-            .reply(200, expectedResponse);
+        // nock(baseUrl)
+        //     .get("/v2/customers/my/billing_history")
+        //     .reply(200, expectedResponse);
 
         const getResp = await client.v2.customers.my.billing_history.get();
         expect(getResp).not.toBeNull();
@@ -62,9 +63,9 @@ describe("Integration Test for Billing", () => {
             ],
         };
 
-        nock(baseUrl)
-            .get("/v2/customers/my/billing_history")
-            .reply(200, expectedResponse);
+        // nock(baseUrl)
+        //     .get("/v2/customers/my/billing_history")
+        //     .reply(200, expectedResponse);
 
         const getResp = await client.v2.customers.my.billing_history.get();
         expect(getResp).not.toBeNull();
@@ -79,9 +80,9 @@ describe("Integration Test for Billing", () => {
     it("should get invoice CSV by UUID", async () => {
         const expectedResponse = "product,group_description,";
 
-        nock(baseUrl)
-            .get(`/v2/customers/my/invoices/${invoiceUuid}/csv`)
-            .reply(200, expectedResponse);
+        // nock(baseUrl)
+        //     .get(`/v2/customers/my/invoices/${invoiceUuid}/csv`)
+        //     .reply(200, expectedResponse);
 
         const getResp = await client.v2.customers.my.invoices.byInvoice_uuid(invoiceUuid).csv.get();
         expect(getResp).not.toBeNull();
@@ -92,9 +93,9 @@ describe("Integration Test for Billing", () => {
     it("should get invoice PDF by UUID", async () => {
         const expectedResponse = Buffer.from("mock-pdf-content");
 
-        nock(baseUrl)
-            .get(`/v2/customers/my/invoices/${invoiceUuid}/pdf`)
-            .reply(200, expectedResponse);
+        // nock(baseUrl)
+        //     .get(`/v2/customers/my/invoices/${invoiceUuid}/pdf`)
+        //     .reply(200, expectedResponse);
 
         const getResp = await client.v2.customers.my.invoices.byInvoice_uuid(invoiceUuid).pdf.get();
         expect(getResp).not.toBeNull();
@@ -107,16 +108,12 @@ describe("Integration Test for Billing", () => {
     });
 
     it("should get invoice summary by UUID", async () => {
-        const expectedResponse = {
-            user_company: "DigitalOcean",
-        };
-
-        nock(baseUrl)
-            .get(`/v2/customers/my/invoices/${invoiceUuid}/summary`)
-            .reply(200, expectedResponse);
+  
+        // nock(baseUrl)
+        //     .get(`/v2/customers/my/invoices/${invoiceUuid}/summary`)
+        //     .reply(200, expectedResponse);
 
         const getResp = await client.v2.customers.my.invoices.byInvoice_uuid(invoiceUuid).summary.get();
         expect(getResp).not.toBeNull();
-        expect(getResp?.userCompany).toBe("DigitalOcean");
     });
 });
