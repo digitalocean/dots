aa648ea71f17e87ce98fb1d844fbcaee23664adb
danaelhe <42972711+danaelhe@users.noreply.github.com>
2025-08-19 14:31:38 -0400

    package.json main should be index.js (#52)
    * package.json main should be index.js
    
    * Update index.ts
    
    * Update README.md

---
+++ index.ts
@@ -0,0 +1,4 @@
+export { createDigitalOceanClient } from "./src/dots/digitalOceanClient.js";
+export { DigitalOceanApiKeyAuthenticationProvider } from "./src/dots/DigitalOceanApiKeyAuthenticationProvider.js";
+export { FetchRequestAdapter } from "@microsoft/kiota-http-fetchlibrary";
+export * from "./src/dots/models/index.js";
